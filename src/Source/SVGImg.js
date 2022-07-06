634946ebbf7ffa1ae2d48dca48e7467b68f8f8e5
salonisodagar <salonisodagar.vhits@gmail.com>
2022-07-06 19:05:56 +0530

    add svg icon

--- src/Source/SVGImg.js
+++ src/Source/SVGImg.js
@@ -1,49 +1,54 @@
-import SplashLogo from "./SplashLogo"
-import Check from "./Check"
-import SideBar from "./SideBar"
-import HeaderLogo from './HeaderLogo'
-import SideBarLogo from "./SideBarLogo"
-import HomeIcon from "./HomeIcon"
-import Profile from "./Profile"
-import StudentVoice from "./StudentVoice"
-import Technical from "./Technical"
-import Guidance from "./Guidance"
-import LogOut from "./LogOut"
-import Close from "./Close"
-import TabSurvey from './TabSurvey'
-import TaBNot from './TaBNot'
-import TabResult from './TabResult'
-import TabServeyWhite from './TabServeyWhite'
-import TabResultWhite from './TabResultWhite'
-import TabNotWhite from './TabNotWhite'
-import Notification from "./Notification"
-import Arrow from "./Arrow"
-import QATBlack from "./QATBlack"
-import Edit from "./Edit"
-import EmailIcon from "./EmailIcon"
+import SplashLogo from './SplashLogo';
+import Check from './Check';
+import SideBar from './SideBar';
+import HeaderLogo from './HeaderLogo';
+import SideBarLogo from './SideBarLogo';
+import HomeIcon from './HomeIcon';
+import Profile from './Profile';
+import StudentVoice from './StudentVoice';
+import Technical from './Technical';
+import Guidance from './Guidance';
+import LogOut from './LogOut';
+import Close from './Close';
+import TabSurvey from './TabSurvey';
+import TaBNot from './TaBNot';
+import TabResult from './TabResult';
+import TabServeyWhite from './TabServeyWhite';
+import TabResultWhite from './TabResultWhite';
+import TabNotWhite from './TabNotWhite';
+import Notification from './Notification';
+import Arrow from './Arrow';
+import QATBlack from './QATBlack';
+import Edit from './Edit';
+import EmailIcon from './EmailIcon';
+import Search from './Search';
+import DownArrow from './DownArrow';
+import UpArrow from './UpArrow';
 export default {
-    SplashLogo: SplashLogo,
-    Check: Check,
-    SideBar: SideBar,
-    HeaderLogo: HeaderLogo,
-    SideBarLogo: SideBarLogo,
-    HomeIcon: HomeIcon,
-    Profile: Profile,
-    StudentVoice: StudentVoice,
-    Technical: Technical,
-    Guidance: Guidance,
-    LogOut: LogOut,
-    Close: Close,
-    TabSurvey: TabSurvey,
-    TaBNot: TaBNot,
-    TabResult: TabResult,
-    TabServeyWhite: TabServeyWhite,
-    TabResultWhite: TabResultWhite,
-    TabNotWhite: TabNotWhite,
-    Notification: Notification,
-    Arrow: Arrow,
-    QATBlack: QATBlack,
-    Edit: Edit,
-    EmailIcon: EmailIcon
-
-}
+  SplashLogo: SplashLogo,
+  Check: Check,
+  SideBar: SideBar,
+  HeaderLogo: HeaderLogo,
+  SideBarLogo: SideBarLogo,
+  HomeIcon: HomeIcon,
+  Profile: Profile,
+  StudentVoice: StudentVoice,
+  Technical: Technical,
+  Guidance: Guidance,
+  LogOut: LogOut,
+  Close: Close,
+  TabSurvey: TabSurvey,
+  TaBNot: TaBNot,
+  TabResult: TabResult,
+  TabServeyWhite: TabServeyWhite,
+  TabResultWhite: TabResultWhite,
+  TabNotWhite: TabNotWhite,
+  Notification: Notification,
+  Arrow: Arrow,
+  QATBlack: QATBlack,
+  Edit: Edit,
+  EmailIcon: EmailIcon,
+  Search: Search,
+  DownArrow: DownArrow,
+  UpArrow: UpArrow,
+};

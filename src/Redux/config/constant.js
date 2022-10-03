0eafff27c7d31c85227438e006942397ce643e4f
satyam-vhits <satyam.vhits@gmail.com>
2022-10-03 19:31:14 +0530

    Change base url

--- src/Redux/config/constant.js
+++ src/Redux/config/constant.js
@@ -1,4 +1,4 @@
 export default {
-  BASE_URL: 'https://appworkdemo.com/STUVO/public/api/v3/', //example
+  BASE_URL: 'https://php74.appworkdemo.com/STUVO/public/api/v3/', //example https://php74.appworkdemo.com/STUVO/public/login
   webLink: 'https://php8.appworkdemo.com/simplesamlphp/', // azure login webview
 };
